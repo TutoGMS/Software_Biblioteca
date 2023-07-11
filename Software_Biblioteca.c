e91b775a44a13a1f109e11dbadb42b9aac3dc594
TutoGMS <arthurgms@usp.br>
2023-07-10 22:20:59 -0300

    commit inicial

---
+++ Software_Biblioteca.c
@@ -0,0 +1,11 @@
+
+/*  Software para o trabalho final de laboratório de ICC, professor Jó
+    Alunos: Arthur Gomes Mesquita/ Allan Vitor/ Matheus Aguiar  */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    
+    return 0;
+}
